54a164167218b0471307b1f5d38bfb5b6626f46b
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-05-19 22:15:23 +0600

    change varcel json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const app =express();
 const port =process.env.PORT || 5000;
 
@@ -24,7 +24,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
       const toyCarCollection =client.db('toyCarDB').collection('toyCar');
 
@@ -49,8 +48,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
