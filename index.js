8f00dd7f686ddb8f839902b20fcf37e3602476c9
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-05-19 10:53:52 +0600

    give me a post data

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS)
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xnjeavl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +25,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+      const toyCarCollection =client.db('toyCarDB').collection('toyCar');
+
+    app.get('/toyCars', async(req, res)=>{
+      const cursor = toyCarCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+   
+
+    app.post('/toyCars', async(req, res)=>{
+      const newToyCar =req.body;
+      console.log(newToyCar)
+      const result = await toyCarCollection.insertOne(newToyCar)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
